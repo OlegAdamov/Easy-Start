1028ea8975ab5d344677a9d9653da676d09a58c2
Viktor Petryshyn <v.petrishyn@gmail.com>
2022-09-27 18:02:24 +0300

    work on GET request

--- src/js/feach/FilmAPIService.js
+++ src/js/feach/FilmAPIService.js
@@ -1,13 +1,24 @@
-//   api_key: 'ba12bbb2efd4020faab2c5dd14dc19c0'
 import axios from 'axios';
+const BASE_UR = 'https://api.themoviedb.org/3/';
+// const API_KEY = 'ba12bbb2efd4020faab2c5dd14dc19c0';
 export default class FilmAPIService {
-
-    constructor() { 
-        this.page = 1
-    }
-    getPopularMovie() {
-        return axios.get(
-            `https://api.themoviedb.org/3/trending/movie/week?api_key=ba12bbb2efd4020faab2c5dd14dc19c0&page=${this.page}`
-        )
-    }
+  constructor() {
+    this.page = 1;
+    this.moviId = 0;
+  }
+  async getPopularMovie() {
+    return await axios.get(
+      `${BASE_UR}trending/movie/week?api_key=ba12bbb2efd4020faab2c5dd14dc19c0&page=${this.page}`
+    );
+  }
+  async getDetail() {
+    return await axios.get(
+      `${BASE_UR}${this.movieId}?api_key=ba12bbb2efd4020faab2c5dd14dc19c0&language=en-US`
+    );
+  }
+  async getGenres() {
+    return await axios.get(
+      `${BASE_UR}genre/movie/list?api_key=ba12bbb2efd4020faab2c5dd14dc19c0`
+    );
+  }
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,19 +1,36 @@
 import FilmAPIService from './feach/FilmAPIService';
-const galleryRefs = document.querySelector('.gallery');
 const filmAPIService = new FilmAPIService();
 
-filmAPIService
-  .getPopularMovie()
-  .then(res => {
-    console.log(res);
-    renderGalleryMarkup(res);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  searchForm: document.getElementById('search-form'),
+};
+async function getResponseMovie() {
+  const response = await filmAPIService.getPopularMovie();
+  const movies = await response.data.results;
+  console.log('getResponseMovie ~ v', movies);
+  const moviArray = movies.map(({ ids }) => ids);
+  filmAPIService.moviId = moviArray.map(id => id);
+  console.log(filmAPIService.moviId);
+  renderGalleryMarkup(movies);
+
+  // .then(res => {
+  //   console.log(res);
+  //   renderGalleryMarkup(res);
+  // })
+  // .catch(error => {
+  //   console.log(error);
+  // });
+}
+getResponseMovie();
+
+function renderGalleryMarkup(res) {
+  const markup = createGalleryMarkup(res);
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
 
 function createGalleryMarkup(res) {
-  return res.data.results
+  return res
     .map(
       ({
         id,
@@ -23,7 +40,7 @@ function createGalleryMarkup(res) {
       }) => ` <li id ="${id}" class="gallery__item">
         <a href="/" class="gallery__link">
             <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" class="gallery__img">
-           
+
            <h2 class="gallery__title">${title}</h2>
             <div class ="discription"><p class="gallery__discription">${release_date}</p>
             </div>
@@ -31,9 +48,4 @@ function createGalleryMarkup(res) {
     </li>`
     )
     .join(' ');
-}
-
-function renderGalleryMarkup(res) {
-  const markup = createGalleryMarkup(res);
-  galleryRefs.insertAdjacentHTML('beforeend', markup);
 }
