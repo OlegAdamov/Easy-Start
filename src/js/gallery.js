f26d29e3740a720a19350eaf04f46f3f8c2bcb2f
Viktor Petryshyn <v.petrishyn@gmail.com>
2022-09-27 16:41:34 +0300

    test

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,17 +1,26 @@
+import FilmAPIService from './feach/FilmAPIService';
+const galleryRefs = document.querySelector('.gallery');
+const filmAPIService = new FilmAPIService();
 
-import FilmAPIService from "./feach/FilmAPIService";
-const galleryRefs = document.querySelector('.gallery')
-const filmAPIService = new FilmAPIService()
-
-filmAPIService.getPopularMovie().then(res => {
+filmAPIService
+  .getPopularMovie()
+  .then(res => {
     console.log(res);
-    renderGalleryMarkup(res)
-}).catch(error => {
-    console.log(error)
-})
+    renderGalleryMarkup(res);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 function createGalleryMarkup(res) {
-    return res.data.results.map(({id,poster_path, title, release_date})=>` <li id ="${id}" class="gallery__item">
+  return res.data.results
+    .map(
+      ({
+        id,
+        poster_path,
+        title,
+        release_date,
+      }) => ` <li id ="${id}" class="gallery__item">
         <a href="/" class="gallery__link">
             <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" class="gallery__img">
            
@@ -19,10 +28,12 @@ function createGalleryMarkup(res) {
             <div class ="discription"><p class="gallery__discription">${release_date}</p>
             </div>
         </a>
-    </li>`).join(' ')
+    </li>`
+    )
+    .join(' ');
 }
 
 function renderGalleryMarkup(res) {
-    const markup = createGalleryMarkup(res)
-    galleryRefs.insertAdjacentHTML('beforeend', markup)
+  const markup = createGalleryMarkup(res);
+  galleryRefs.insertAdjacentHTML('beforeend', markup);
 }
