25528137d335349558cc75ae2a84b8f9d3f33b34
Oleksandr <alexkrekoten11@gmail.com>
2022-09-27 12:00:22 +0300

    feature/APIService

---
+++ src/js/gallery.js
@@ -0,0 +1,28 @@
+
+import FilmAPIService from "./feach/FilmAPIService";
+const galleryRefs = document.querySelector('.gallery')
+const filmAPIService = new FilmAPIService()
+
+filmAPIService.getPopularMovie().then(res => {
+    console.log(res);
+    renderGalleryMarkup(res)
+}).catch(error => {
+    console.log(error)
+})
+
+function createGalleryMarkup(res) {
+    return res.data.results.map(({id,poster_path, title, release_date})=>` <li id ="${id}" class="gallery__item">
+        <a href="/" class="gallery__link">
+            <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" class="gallery__img">
+           
+           <h2 class="gallery__title">${title}</h2>
+            <div class ="discription"><p class="gallery__discription">${release_date}</p>
+            </div>
+        </a>
+    </li>`).join(' ')
+}
+
+function renderGalleryMarkup(res) {
+    const markup = createGalleryMarkup(res)
+    galleryRefs.insertAdjacentHTML('beforeend', markup)
+}
