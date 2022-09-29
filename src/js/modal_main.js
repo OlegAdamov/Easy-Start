aacfbe5acffed6d8f368be0ade1c21a09b70eb46
belyy10 <maximgyda10@icloud.com>
2022-09-29 22:43:05 +0300

    responsive

--- src/js/modal_main.js
+++ src/js/modal_main.js
@@ -1,7 +1,15 @@
+import { createGalleryMarkup } from "./gallery";
+
 const refs = {
-    linkEl: document.querySelector('.gallery-link'),
-    titleEl: document.querySelector('.gallery__title'),
-    imgEl: document.querySelector('gallery__img'),
     galleryRef: document.querySelector('.gallery'),
-    
+    closeBtn: document.querySelector('[modal-close-btn]'),
+    modal: document.querySelector('.modal-main'),
+    galleryItem: document.querySelector('.gallery__item')
+}
+console.log(refs.galleryRef)
+console.log(refs.modal)
+refs.closeBtn.addEventListener('click',removeBtn)
+
+function removeBtn () {
+    refs.modal.classList.add ('is-hidden');
 }
