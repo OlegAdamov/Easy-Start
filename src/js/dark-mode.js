651a92eaba0ab08e47885e676964046d680c08e4
Oleksandr <alexkrekoten11@gmail.com>
2022-09-29 12:54:33 +0300

    save checkbox to localStoraage

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,9 +1,39 @@
-const icon = document.querySelector('.icon')
-const body = document.querySelector('body')
-console.log(icon);
-console.log(body);
-icon.addEventListener('click', onChangeColor)
+// const icon = document.querySelector('.icon')
+// const body = document.querySelector('body')
+// console.log(icon);
+// console.log(body);
+// icon.addEventListener('click', onChangeColor)
 
-function onChangeColor() {
-  body.classList.toggle('darkTheme')  
+// function onChangeColor() {
+//     body.classList.toggle('darkTheme')
+      
+// }
+window.addEventListener('load', onLoad)
+const toggle = document.getElementById('switch')
+
+
+toggle.addEventListener('click', darkMode)
+
+function darkMode() {
+    const body = document.querySelector('body');
+    const wasDarkmode = localStorage.getItem('darkmode')=== 'true'
+    localStorage.setItem('darkmode', !wasDarkmode)
+    body.classList.toggle('darkTheme', !wasDarkmode)
+    saveCheckbox()
 }
+
+function onLoad() {
+    document.body.classList.toggle('darkTheme', localStorage.getItem('darkmode') === 'true')
+    
+    
+}
+let checked = JSON.parse(localStorage.getItem('checkbox1zaal1'));
+if (checked == true) {
+    document.getElementById("switch").checked = true;
+} 
+
+function saveCheckbox(){
+    let checkbox = document.querySelector('#switch');
+    localStorage.setItem('checkbox1zaal1', checkbox.checked);
+}
+
