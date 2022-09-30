bd6712c1cf726f38da1794234792f55acc7b946c
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-09-30 17:32:39 +0300

    Pagination-1

--- src/js/feach/FilmAPIService.js
+++ src/js/feach/FilmAPIService.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 const BASE_UR = 'https://api.themoviedb.org/3/';
-// const API_KEY = 'ba12bbb2efd4020faab2c5dd14dc19c0';
+const API_KEY = 'ba12bbb2efd4020faab2c5dd14dc19c0';
 export default class FilmAPIService {
   constructor() {
     this.query = '';
     this.page = 1;
     this.genres = [];
   }
+
   async getPopularMovie() {
     return await axios.get(
       `${BASE_UR}trending/movie/week?api_key=ba12bbb2efd4020faab2c5dd14dc19c0&page=${this.page}`
