371b7744fb89fa5cc9cc1714c569f69253a302a0
Oleksandr <alexkrekoten11@gmail.com>
2022-10-03 12:46:53 +0300

    bugfix/loader

---
+++ src/js/loader.js
@@ -0,0 +1,8 @@
+export default function loader() {
+    const loader = document.getElementById('preloader')
+    loader.style.display = 'flex'
+    setTimeout(() => {
+       loader.style.display = 'none'
+    }, 800);
+}
+
